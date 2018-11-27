8b88c84dfdcec541c0d643b8092f47bbb140ef18
Michael Angerman <stormasm@gmail.com>
2018-11-27 13:45:30 -0800

    docker user

--- adduser.bash
+++ adduser.bash
@@ -34,13 +34,18 @@ echo 'AllowUsers root michael' >> sshd_config
 # Any time you change the sshd_config file you must run this command:
 sudo systemctl restart sshd.service
 
-#Then add them to the sudoers group
+# Then add them to the sudoers group
 usermod -aG sudo michael
 
 cp /tmp/.bash_aliases /home/michael
 cp /tmp/id_rsa* /home/michael/.ssh
 git config --global user.email stormasm@gmail.com
 git config --global user.name "Michael Angerman"
+
+# This is for docker, for now you still need to do a sudo reboot...
+# until you figure out how to restart the docker daemon
+sudo groupadd docker
+sudo usermod -aG docker michael
 
 # Relevant Links:
 # https://help.ubuntu.com/lts/serverguide/user-management.html.en
