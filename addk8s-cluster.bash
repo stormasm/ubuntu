8fa08ebd977f2563bd6e4599a8ae898ab0b79585
Michael Angerman <stormasm@gmail.com>
2018-11-28 13:21:57 -0800

    add some comments

--- addk8s-cluster.bash
+++ addk8s-cluster.bash
@@ -1,4 +1,12 @@
+#
+# Run these commands on master prior to running any
+#
+# kubeadm join commands on the client nodes...
+
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+
+# This is an example of an addon you can run...
+# But you do not need to run any addons.
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
