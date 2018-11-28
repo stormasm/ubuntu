0b9ef2361af3963f5fc2b44bab9458780fc0ae0a
Michael Angerman <stormasm@gmail.com>
2018-11-27 18:10:42 -0800

    initial commit of adddocker.bash

---
+++ adddocker.bash
@@ -0,0 +1,38 @@
+# Install Docker from Ubuntu's repositories:
+# apt-get update
+# apt-get install -y docker.io
+
+# or install Docker CE 18.06 from Docker's repositories for Ubuntu or Debian:
+
+## Install prerequisites.
+apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+## Download GPG key.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+## Add docker apt repository.
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+## Install docker.
+apt-get update && apt-get install docker-ce=18.06.0~ce~3-0~ubuntu
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
