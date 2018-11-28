110f19a5da7da5de9a0d8451bf7cdb9961903539
Michael Angerman <stormasm@gmail.com>
2018-11-27 18:24:22 -0800

    addcrio.bash initial commit

---
+++ addcrio.bash
@@ -0,0 +1,30 @@
+
+#
+# For more info:
+# https://kubernetes.io/docs/setup/cri/#cri-o
+#
+
+modprobe overlay
+modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+# Install prerequisites
+apt-get update
+apt-get -y install software-properties-common
+
+add-apt-repository ppa:projectatomic/ppa
+apt-get update
+
+# Install CRI-O
+apt-get -y install cri-o-1.11
+
+# Start CRI-O
+systemctl start crio
